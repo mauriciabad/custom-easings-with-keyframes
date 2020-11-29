28c511002fbf94845556167024be657acc23f6d7
Maurici Abad <maurici.abad@glovoapp.com>
2020-11-29 21:21:08 +0100

    Create main components

--- src/components/KeyframesCanvas.vue
+++ src/components/KeyframesCanvas.vue
@@ -1,17 +1,55 @@
 <script lang="ts">
 import KeyframesCanvasPoint from '@/components/KeyframesCanvasPoint.vue'
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+
+interface Point {
+  x: number
+  y: number
+  isSelected: boolean
+}
 
 export default defineComponent({
   components: { KeyframesCanvasPoint },
-  props: {}
+  props: {},
+
+  setup() {
+    const points = ref<Point[]>([
+      {
+        x: 100,
+        y: 100,
+        isSelected: false
+      },
+      {
+        x: 200,
+        y: 100,
+        isSelected: false
+      }
+    ])
+
+    const handlePointClick = (x: number) => {
+      points.value = points.value.map(p => ({ ...p, isSelected: false }))
+
+      const point = points.value.find(p => p.x === x)
+
+      if (point) {
+        point.isSelected = true
+      }
+    }
+
+    return { points, handlePointClick }
+  }
 })
 </script>
 
 <template>
   <div class="canvas-container">
-    <svg viewBox="0 0 1000 800">
-      <keyframes-canvas-point :x="100" :y="100" />
+    <svg viewBox="0 0 1000 800" class="canvas">
+      <keyframes-canvas-point
+        v-for="point in points"
+        :key="point.x"
+        :point="point"
+        @click="handlePointClick(point.x)"
+      />
     </svg>
   </div>
 </template>
@@ -19,5 +57,9 @@ export default defineComponent({
 <style scoped lang="scss">
 .canvas-container {
   width: 100%;
+}
+.canvas {
+  border: solid 4px #333;
+  border-radius: 4px;
 }
 </style>
--- src/components/KeyframesCanvasPoint.vue
+++ src/components/KeyframesCanvasPoint.vue
@@ -1,16 +1,67 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+interface Point {
+  x: number
+  y: number
+  isSelected: boolean
+}
+
 export default defineComponent({
-  props: {
-    x: Number,
-    y: Number
+  props: { point: Object as () => Point },
+  emits: ['click'],
+
+  setup() {
+    return {}
   }
 })
 </script>
 
 <template>
-  <rect :x="x" :y="y" width="10" height="10" fill="#00f" />
+  <rect
+    :x="point.x - 4"
+    :y="point.y - 4"
+    width="22"
+    height="22"
+    rx="6"
+    fill="#FF00A8"
+    opacity="0.5"
+    class="point-outline"
+    :class="{ 'point-outline--selected': point.isSelected }"
+  />
+
+  <rect
+    :x="point.x"
+    :y="point.y"
+    width="14"
+    height="14"
+    rx="2"
+    fill="#FFED48"
+    stroke="black"
+    stroke-width="2"
+    tabindex="0"
+    class="point"
+    :class="{ 'point--selected': point.isSelected }"
+    @click="$emit('click', $event)"
+  />
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.point-outline {
+  pointer-events: none;
+  display: none;
+
+  &--selected {
+    display: block;
+  }
+}
+.point {
+  cursor: pointer;
+  outline: none;
+
+  &--selected {
+    cursor: move;
+    outline: none;
+  }
+}
+</style>
