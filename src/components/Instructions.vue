46956e30f2b0459903fa571b9ac663b4b5e5ea1c
Maurici Abad <maurici.abad@glovoapp.com>
2021-01-02 23:17:11 +0100

    Instructions style

--- src/components/Instructions.vue
+++ src/components/Instructions.vue
@@ -1,7 +1,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Icon from '@/components/Icon.vue'
 
 export default defineComponent({
+  components: { Icon },
   props: {},
 
   setup() {
@@ -14,22 +16,40 @@ export default defineComponent({
   <div class="instructions">
     <h2>Instructions</h2>
     <ul>
-      <li>Click => Add point / Select point</li>
-      <li>Right click / Delete key => Delete selected points</li>
-      <li>Drag => Move selected points</li>
-      <li>Shift + Click => Select many points</li>
+      <li>
+        <icon name="left-click" /><icon name="right-arrow" /> Add point / Select
+        point
+      </li>
+      <li>
+        <icon name="move" /><icon name="right-arrow" /> Move selected points
+      </li>
+      <li>
+        <icon name="right-click" />/<icon name="key-backspace" /><icon
+          name="right-arrow"
+        />
+        Delete selected points
+      </li>
+      <li>
+        <icon name="key-shift" />+<icon name="left-click" /><icon
+          name="right-arrow"
+        />
+        Select many points
+      </li>
     </ul>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .instructions {
-  border: solid 4px #333;
+  border: solid 1px #ddd;
   padding: 1.75rem 2rem;
   border-radius: 1rem;
+  box-shadow: 0 2px 16px rgba(0, 0, 0, 0.15);
 
   h2 {
-    margin: 0 0 1rem;
+    margin: 0 0 1.5rem;
+    line-height: 0.8;
+    text-transform: uppercase;
   }
 
   ul {
@@ -39,6 +59,10 @@ export default defineComponent({
 
     li {
       margin-bottom: 1rem;
+
+      svg:first-child {
+        margin-left: -0.5rem;
+      }
 
       &:last-child {
         margin-bottom: 0;
