56969b1d8790831859ee39258a902cf933374557
Maurici Abad <maurici.abad@glovoapp.com>
2021-01-06 17:59:36 +0100

    Remove space

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -26,9 +26,8 @@ export default defineComponent({
       >
         <path
           d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-        />
-      </svg>
-      Star project</a
+        /></svg
+      >Star project</a
     >
 
     <a
@@ -46,9 +45,8 @@ export default defineComponent({
           fill-rule="evenodd"
           d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z"
           clip-rule="evenodd"
-        />
-      </svg>
-      Donate</a
+        /></svg
+      >Donate</a
     >
     <a
       href="https://www.buymeacoffee.com/mauriciabad"
