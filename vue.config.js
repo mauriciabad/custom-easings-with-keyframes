bee80a898eb3038b40ecc0d2bbea568c2518aa9f
Maurici Abad <maurici.abad@glovoapp.com>
2021-01-09 16:29:42 +0100

    Update pwa icons 2

--- vue.config.js
+++ vue.config.js
@@ -19,13 +19,13 @@ module.exports = {
           purpose: 'any'
         },
         {
-          src: '/img/icons/android-chrome-192x192.png',
+          src: '/img/icons/maskable-192x192.png',
           sizes: '192x192',
           type: 'image/png',
           purpose: 'maskable'
         },
         {
-          src: '/img/icons/android-chrome-512x512.png',
+          src: '/img/icons/maskable-512x512.png',
           sizes: '512x512',
           type: 'image/png',
           purpose: 'maskable'
@@ -46,6 +46,7 @@ module.exports = {
       appleTouchIcon: 'img/icons/apple-touch-icon.png',
       maskIcon: 'img/icons/safari-pinned-tab.svg',
       msTileImage: 'img/icons/msapplication-icon-144x144.png'
-    }
+    },
+    msTileColor: '#719dff'
   }
 }
