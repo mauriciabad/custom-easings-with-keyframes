e34819c3d5f7785d721c21fa59b75885219633da
Maurici Abad <maurici.abad@glovoapp.com>
2021-01-09 16:39:04 +0100

    Rename files

--- vue.config.js
+++ vue.config.js
@@ -19,13 +19,13 @@ module.exports = {
           purpose: 'any'
         },
         {
-          src: '/img/icons/maskable-192x192.png',
+          src: '/img/icons/android-chrome-maskable-192x192.png',
           sizes: '192x192',
           type: 'image/png',
           purpose: 'maskable'
         },
         {
-          src: '/img/icons/maskable-512x512.png',
+          src: '/img/icons/android-chrome-maskable-512x512.png',
           sizes: '512x512',
           type: 'image/png',
           purpose: 'maskable'
