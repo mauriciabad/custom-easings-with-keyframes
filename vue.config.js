d260f86bc40fb863e9e3f3efa9128dc27be0c94f
Maurici Abad <maurici.abad@glovoapp.com>
2021-01-09 16:16:49 +0100

    Update pwa icons

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,6 @@
 
 module.exports = {
   pwa: {
-    // name: 'Keyframes easing',
-    // themeColor: '#719dff',
-    // msTileColor: '#719dff',
     manifestOptions: {
       name: 'Keyframes easing',
       short_name: 'Keyframes easing',
@@ -12,17 +9,43 @@ module.exports = {
         {
           src: '/img/icons/android-chrome-192x192.png',
           sizes: '192x192',
-          type: 'image/png'
+          type: 'image/png',
+          purpose: 'any'
         },
         {
           src: '/img/icons/android-chrome-512x512.png',
           sizes: '512x512',
-          type: 'image/png'
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: '/img/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable'
+        },
+        {
+          src: '/img/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        },
+        {
+          src: '/img/icons/safari-pinned-tab.svg',
+          type: 'image/svg',
+          purpose: 'monochrome'
         }
       ],
       theme_color: '#719dff',
       background_color: '#ffffff',
       display: 'standalone'
+    },
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
     }
   }
 }
