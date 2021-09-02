874b3c9053f0c2a3610c70b024dc370bbeecae3b
Maurici Abad <maurici.abad@glovoapp.com>
2021-09-02 03:12:29 +0200

    Add deletion tests

--- cypress/integration/canvas.spec.ts
+++ cypress/integration/canvas.spec.ts
@@ -6,11 +6,12 @@ import dedent from 'dedent'
  * @param x From 0 to 1
  * @param y From 0 to 1
  */
-export function clickInCanvas(
+function clickInCanvas(
   canvas: Cypress.Chainable<JQuery<HTMLElement>>,
   x: number,
   y: number,
-  clickType: 'left' | 'right' = 'left'
+  clickType: 'left' | 'right' = 'left',
+  options: Partial<Cypress.ClickOptions> = {}
 ) {
   return canvas.then(element => {
     const canvasBox = element[0].getBoundingClientRect()
@@ -22,16 +23,32 @@ export function clickInCanvas(
     switch (clickType) {
       case 'right':
         return canvas.rightclick(canvasClickPoints.x, canvasClickPoints.y, {
-          force: true
+          force: true,
+          ...options
         })
 
       case 'left':
       default:
         return canvas.click(canvasClickPoints.x, canvasClickPoints.y, {
-          force: true
+          force: true,
+          ...options
         })
     }
   })
+}
+
+function expectCodeToBeTheInitialState(
+  code: Cypress.Chainable<JQuery<HTMLElement>>
+) {
+  return code.contains(dedent/* css */ `
+      .ease-custom {
+        animation: ease-custom 3000ms linear;
+      }
+
+      @keyframes ease-custom {
+        0% {transform: rotate(0deg)}
+        100% {transform: rotate(360deg)}
+      }`)
 }
 
 describe('Canvas element', () => {
@@ -43,18 +60,11 @@ describe('Canvas element', () => {
     cy.contains('Get started').click()
 
     cy.get('[data-test-id="canvas-main-area"]').as('canvas')
+    cy.get('pre').as('code')
   })
 
   it('has default code', () => {
-    cy.get('pre').contains(dedent/* css */ `
-      .ease-custom {
-        animation: ease-custom 3000ms linear;
-      }
-
-      @keyframes ease-custom {
-        0% {transform: rotate(0deg)}
-        100% {transform: rotate(360deg)}
-      }`)
+    expectCodeToBeTheInitialState(cy.get('@code'))
   })
 
   describe('and adds a new point', () => {
@@ -63,7 +73,7 @@ describe('Canvas element', () => {
     })
 
     it('adds a new keyframe to the code', () => {
-      cy.get('pre').contains(dedent/* css */ `
+      cy.get('@code').contains(dedent/* css */ `
       .ease-custom {
         animation: ease-custom 3000ms linear;
       }
@@ -75,21 +85,99 @@ describe('Canvas element', () => {
       }`)
     })
 
-    describe('and right clicks on the point', () => {
+    describe('and adds another point holding shift', () => {
       beforeEach(() => {
-        clickInCanvas(cy.get('@canvas'), 0.5, 0.25, 'right')
+        clickInCanvas(cy.get('@canvas'), 0.75, 0.5, 'left', {
+          shiftKey: true
+        })
       })
 
-      it('removes a keyframe from the code', () => {
-        cy.get('pre').contains(dedent/* css */ `
+      it('adds a new keyframe to the code', () => {
+        cy.get('@code').contains(dedent/* css */ `
       .ease-custom {
         animation: ease-custom 3000ms linear;
       }
 
       @keyframes ease-custom {
         0% {transform: rotate(0deg)}
+        50% {transform: rotate(90deg)}
+        75% {transform: rotate(180deg)}
         100% {transform: rotate(360deg)}
       }`)
+      })
+
+      describe('and clicks the delete key', () => {
+        beforeEach(() => {
+          cy.get('@canvas').trigger('keydown', {
+            key: 'Backspace',
+            force: true
+          })
+        })
+
+        it('removes the 2 keyframes from the code', () => {
+          expectCodeToBeTheInitialState(cy.get('@code'))
+        })
+      })
+    })
+
+    describe('and adds another point', () => {
+      beforeEach(() => {
+        clickInCanvas(cy.get('@canvas'), 0.75, 0.5)
+      })
+
+      it('adds a new keyframe to the code', () => {
+        cy.get('@code').contains(dedent/* css */ `
+      .ease-custom {
+        animation: ease-custom 3000ms linear;
+      }
+
+      @keyframes ease-custom {
+        0% {transform: rotate(0deg)}
+        50% {transform: rotate(90deg)}
+        75% {transform: rotate(180deg)}
+        100% {transform: rotate(360deg)}
+      }`)
+      })
+
+      describe('and selects the first point', () => {
+        beforeEach(() => {
+          clickInCanvas(cy.get('@canvas'), 0.5, 0.25, 'left', {
+            shiftKey: true
+          })
+        })
+
+        describe('and clicks the delete key', () => {
+          beforeEach(() => {
+            cy.get('@canvas').trigger('keydown', {
+              key: 'Backspace',
+              force: true
+            })
+          })
+
+          it('removes the 2 keyframes from the code', () => {
+            expectCodeToBeTheInitialState(cy.get('@code'))
+          })
+        })
+
+        describe('and does a right click', () => {
+          beforeEach(() => {
+            clickInCanvas(cy.get('@canvas'), 0.75, 0.5, 'right')
+          })
+
+          it('removes the 2 keyframes from the code', () => {
+            expectCodeToBeTheInitialState(cy.get('@code'))
+          })
+        })
+      })
+    })
+
+    describe('and right clicks on the point', () => {
+      beforeEach(() => {
+        clickInCanvas(cy.get('@canvas'), 0.5, 0.25, 'right')
+      })
+
+      it('removes a keyframe from the code', () => {
+        expectCodeToBeTheInitialState(cy.get('@code'))
       })
     })
   })
