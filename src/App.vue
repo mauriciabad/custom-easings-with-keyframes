14530b1f48e4bd754fd41116aa347f663f6a37e4
Maurici Abad <maurici.abad@glovoapp.com>
2021-09-04 16:52:30 +0200

    Update banner

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import UpdateBanner from './components/UpdateBanner.vue'
+
+export default defineComponent({
+  components: {
+    UpdateBanner
+  }
+})
+</script>
+
 <template>
   <router-view />
+  <update-banner />
 </template>
 
 <style lang="scss">
@@ -20,15 +32,24 @@ $defaultMonoFonts: ui-monospace, SFMono-Regular, Monaco, Consolas,
   Liberation Mono, Roboto Mono, Courier New, monospace, $defaultEmojiFonts;
 
 html,
-body,
+body {
+  font-family: 'Inter var', $defaultFonts;
+}
 input,
 textarea,
 button {
-  font-family: 'Inter var', $defaultFonts;
+  font-size: inherit;
 }
 body,
 button {
   margin: 0;
+}
+button {
+  cursor: pointer;
+  line-height: 1;
+  border: 0;
+  background: none;
+  color: currentColor;
 }
 code {
   font-family: Menlo, $defaultMonoFonts;
