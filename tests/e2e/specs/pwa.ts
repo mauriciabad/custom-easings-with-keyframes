7afdc9a5e3c67656908aaec28332f34b32623716
Maurici Abad <maurici.abad@glovoapp.com>
2021-09-06 20:49:39 +0200

    tests: Add basic PWA test

---
+++ tests/e2e/specs/pwa.ts
@@ -0,0 +1,12 @@
+describe('PWA', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  describe('when it is first visit', () => {
+    it("doesn't show the update banner", () => {
+      cy.contains('New version available!').should('not.be.visible')
+      cy.contains('New version available!').should('not.be.exist')
+    })
+  })
+})
