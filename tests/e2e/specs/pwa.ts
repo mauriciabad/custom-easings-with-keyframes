345f3b5d221065d8e5131af9595c244a1cde6164
Maurici Abad <maurici.abad@glovoapp.com>
2021-09-06 22:19:45 +0200

    Only test not exists

--- tests/e2e/specs/pwa.ts
+++ tests/e2e/specs/pwa.ts
@@ -5,7 +5,6 @@ describe('PWA', () => {
 
   describe('when it is first visit', () => {
     it("doesn't show the update banner", () => {
-      cy.contains('New version available!').should('not.be.visible')
       cy.contains('New version available!').should('not.be.exist')
     })
   })
