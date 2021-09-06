26a620a2541dea44aeaac2f92912c50492c99ac2
Maurici Abad <maurici.abad@glovoapp.com>
2021-09-06 21:42:34 +0200

    Move clickInCanvas

--- tests/e2e/specs/canvas.spec.ts
+++ tests/e2e/specs/canvas.spec.ts
@@ -1,52 +1,5 @@
 import dedent from 'dedent'
-
-/**
- * Cliks in the canvas at the specified coordenates
- * @param canvas Canvas to click in
- * @param x From 0 to 1
- * @param y From 0 to 1
- */
-function clickInCanvas(
-  canvas: Cypress.Chainable<JQuery<HTMLElement>>,
-  x: number,
-  y: number,
-  clickType: 'left' | 'right' | 'hover' = 'left',
-  options: Partial<Cypress.ClickOptions> = {}
-) {
-  return canvas.then((element) => {
-    const canvasBox = element[0].getBoundingClientRect()
-    const canvasClickPoints = {
-      x: x * canvasBox.width,
-      y: canvasBox.height - y * canvasBox.height
-    }
-
-    switch (clickType) {
-      case 'right':
-        return canvas.rightclick(canvasClickPoints.x, canvasClickPoints.y, {
-          force: true,
-          ...options
-        })
-
-      case 'hover':
-        return canvas.trigger(
-          'mouseover',
-          canvasClickPoints.x,
-          canvasClickPoints.y,
-          {
-            force: true,
-            ...options
-          }
-        )
-
-      case 'left':
-      default:
-        return canvas.click(canvasClickPoints.x, canvasClickPoints.y, {
-          force: true,
-          ...options
-        })
-    }
-  })
-}
+import { clickInCanvas } from '../support/interactions'
 
 function expectCodeToBeTheInitialState(
   code: Cypress.Chainable<JQuery<HTMLElement>>
