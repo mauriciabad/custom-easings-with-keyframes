6fa4953b2c0035427a16825cb63433280d6d94d6
Maurici Abad <hello@mauriciabad.com>
2022-01-02 21:37:05 +0100

    fix: Fix sw

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -1,44 +1,36 @@
-/* eslint-disable no-console */
-
 import { register } from 'register-service-worker'
 
 let updateIntervalId: number | undefined = undefined
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    // ready() {
-    //   console.log(
-    //     'App is being served from cache by a service worker.\n' +
-    //       'For more details, visit https://goo.gl/AFskqB'
-    //   )
-    // },
     registered(registration) {
       if (updateIntervalId) clearInterval(updateIntervalId)
+
       updateIntervalId = setInterval(() => {
         registration.update()
-      }, 1000 * 60 * 60) // Check for updates every hour
+      }, 60 * 60 * 1000) // Check for updates every hour
     },
-    // cached() {
-    //   console.log('Content has been cached for offline use.')
-    // },
-    // updatefound() {
-    //   console.log('New content is downloading.')
-    // },
+
     updated(registration) {
-      console.info("There's an update, refresh")
       document.dispatchEvent(
-        new CustomEvent<ServiceWorkerRegistration>('swUpdated', {
-          detail: registration
-        })
+        new CustomEvent<DocumentEventMap['swUpdatedCustomEvent']['detail']>(
+          'swUpdatedCustomEvent',
+          {
+            detail: registration
+          }
+        )
       )
     },
-    // offline() {
-    //   console.log(
-    //     'No internet connection found. App is running in offline mode.'
-    //   )
-    // },
+
     error(error) {
       console.error('Error during service worker registration:', error)
     }
   })
+}
+
+declare global {
+  interface DocumentEventMap {
+    swUpdatedCustomEvent: CustomEvent<ServiceWorkerRegistration>
+  }
 }
