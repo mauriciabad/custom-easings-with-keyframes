e079171ddc5ef7cec7fc1c5192ac3fde377a9f83
Maurici Abad Gutierrez <hello@mauriciabad.com>
2022-01-03 01:05:57 +0100

    refactor: Remove popup teleports (#103)

--- src/components/SmallScreenPopup.vue
+++ src/components/SmallScreenPopup.vue
@@ -18,36 +18,34 @@ export default defineComponent({
 </script>
 
 <template>
-  <teleport to="#small-screen-popup">
-    <transition name="fade">
-      <div v-if="!userAknowladges" class="popup">
-        <div class="background"></div>
-        <div class="popup-container">
-          <div class="icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-              />
-            </svg>
-          </div>
-          <h2>The display is too small</h2>
-          <p>
-            This tool is not optimized for small screens. Open it on a larger
-            screen to use it properly.
-          </p>
-          <button @click="handleClick">I understand, use anyway.</button>
+  <transition name="fade">
+    <div v-if="!userAknowladges" class="popup">
+      <div class="background"></div>
+      <div class="popup-container">
+        <div class="icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+            />
+          </svg>
         </div>
+        <h2>The display is too small</h2>
+        <p>
+          This tool is not optimized for small screens. Open it on a larger
+          screen to use it properly.
+        </p>
+        <button @click="handleClick">I understand, use anyway.</button>
       </div>
-    </transition>
-  </teleport>
+    </div>
+  </transition>
 </template>
 
 <style scoped lang="scss">
