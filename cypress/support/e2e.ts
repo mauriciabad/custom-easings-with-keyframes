a2570b1bea911dbf51b37b7fa380185e659b61d4
mauriciabad <hello@mauriciabad.com>
2022-11-10 01:57:03 +0800

    Revert "Remove error catcher"
    This reverts commit cf441d5a5ba29751423a2a4aebc39c339e77dc11.

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,1 +1,11 @@
 /// <reference types="cypress" />
+
+Cypress.on('uncaught:exception', (err) => {
+  if (err && typeof err.message === 'string') {
+    if (err.message.includes('ResizeObserver')) {
+      return false // returning false prevents Cypress from failing the test
+    }
+  }
+
+  return true
+})
