709d13f8c25414226a17fbe7fa7ef36c3ae4a52f
mauriciabad <hello@mauriciabad.com>
2022-11-09 22:09:18 +0800

    cypresid

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173'
+    baseUrl: 'http://localhost:4173',
+    projectId: "x4o3zk",
+    viewportWidth: 1024,
+    viewportHeight: 768,
   }
 })
