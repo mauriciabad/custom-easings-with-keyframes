0b568a9a9d0194ebf6dfc506ff48556a42b219d3
mauriciabad <hello@mauriciabad.com>
2022-11-09 22:00:08 +0800

    eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,26 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
   'extends': [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier'
   ],
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'vue/component-tags-order': [
+      'error',
+      { order: ['script', 'template', 'style'] }
+    ],
+    'vue/block-lang': ['error', { script: { lang: 'ts' } }],
+    'prefer-template': 'warn',
+    'no-undef': 'off'
+  },
   overrides: [
     {
       files: [
