5ee16524a6e3626d315a78c06e99e7e12389e0bc
mauriciabad <hello@mauriciabad.com>
2022-11-10 01:07:46 +0800

    try to fix cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:4173',
-    projectId: "x4o3zk",
+    projectId: 'x4o3zk',
     viewportWidth: 1024,
     viewportHeight: 768,
-  }
+  },
 })
