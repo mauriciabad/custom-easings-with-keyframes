45aa46e16abf67ab841cb12ca1ec59548366d578
mauriciabad <hello@mauriciabad.com>
2022-11-18 18:10:10 +0800

    Fix label in checkbox coponent

--- src/components/generic/CelebrationCheckbox.vue
+++ src/components/generic/CelebrationCheckbox.vue
@@ -18,8 +18,8 @@ const localValue = computed({
 
 <template>
   <div class="cbx">
+    <!-- eslint-disable-next-line vuejs-accessibility/form-control-has-label -->
     <input :id="id" v-model="localValue" type="checkbox" />
-    <label :for="id"></label>
     <svg width="15" height="14" viewbox="0 0 15 14" fill="none">
       <path d="M2 8.36364L6.23077 12L13 2"></path>
     </svg>
